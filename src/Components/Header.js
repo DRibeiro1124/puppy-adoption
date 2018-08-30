999852fa739ad845622e62ba8bdbb64f0bf0a017
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-29 20:36:44 -0400

    Added two more images to file and placed them in the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import photo1 from "../images/adoptMe.jpeg";
 import photo2 from "../images/adoption_homeless.jpg";
 import photo3 from "../images/adoptMe1.jpeg";
+import photo4 from "../images/Paws.jpg";
 
 class Header extends Component {
     render() {
@@ -11,9 +12,11 @@ class Header extends Component {
                     <span className="intro-title">Pets for Adoption</span>
                 </header>
                 <section className="intro-photo">
+                    <img src={photo4} alt="photo5" className="header-photo" />
                     <img src={photo1} alt="photo1" className="header-photo" />
                     <img src={photo2} alt="photo2" className="header-photo" />
-                    <img src={photo3} alt="photo1" className="header-photo" />
+                    <img src={photo3} alt="photo3" className="header-photo" />
+                    <img src={photo4} alt="photo4" className="header-photo" />
                 </section>
             </div>
         );
